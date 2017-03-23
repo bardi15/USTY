ea8887fb04003f0a44c8e3051765077d401e9fc7
hallur14 <hallur14@ru.is>
2017-03-23 17:12:52 +0000

    major boner

--- ProcessScheduling_bardi15_hallur14/src/com/ru/usty/scheduling/Scheduler.java
+++ ProcessScheduling_bardi15_hallur14/src/com/ru/usty/scheduling/Scheduler.java
@@ -2,104 +2,108 @@ package com.ru.usty.scheduling;
 
 import com.ru.usty.scheduling.process.ProcessExecution;
 
-public class Scheduler {
+import java.util.LinkedList;
+import java.util.Queue;
 
-	ProcessExecution processExecution;
-	Policy policy;
-	int quantum;
+public class Scheduler
+{
 
-	/**
-	 * Add any objects and variables here (if needed)
-	 */
+    ProcessExecution processExecution;
+    Policy policy;
+    int quantum;
+
+    Queue<Integer> processQueue;
 
 
-	/**
-	 * DO NOT CHANGE DEFINITION OF OPERATION
-	 */
-	public Scheduler(ProcessExecution processExecution) {
-		this.processExecution = processExecution;
+    /**
+     * DO NOT CHANGE DEFINITION OF OPERATION
+     */
+    public Scheduler(ProcessExecution processExecution)
+    {
+        this.processExecution = processExecution;
 
-		/**
-		 * Add general initialization code here (if needed)
-		 */
-	}
+        /**
+         * Add general initialization code here (if needed)
+         */
+    }
 
-	/**
-	 * DO NOT CHANGE DEFINITION OF OPERATION
-	 */
-	public void startScheduling(Policy policy, int quantum) {
+    /**
+     * DO NOT CHANGE DEFINITION OF OPERATION
+     */
+    public void startScheduling(Policy policy, int quantum)
+    {
+        this.policy = policy;
+        this.quantum = quantum;
 
-		this.policy = policy;
-		this.quantum = quantum;
+        processQueue = new LinkedList<Integer>();
 
-		/**
-		 * Add general initialization code here (if needed)
-		 */
+        switch (policy)
+        {
+            case FCFS:    //First-come-first-served
+                System.out.println("Starting new scheduling task: First-come-first-served");
+                /**
+                 * Add your policy specific initialization code here (if needed)
+                 */
+                break;
+            case RR:    //Round robin
+                System.out.println("Starting new scheduling task: Round robin, quantum = " + quantum);
+                /**
+                 * Add your policy specific initialization code here (if needed)
+                 */
+                break;
+            case SPN:    //Shortest process next
+                System.out.println("Starting new scheduling task: Shortest process next");
+                /**
+                 * Add your policy specific initialization code here (if needed)
+                 */
+                break;
+            case SRT:    //Shortest remaining time
+                System.out.println("Starting new scheduling task: Shortest remaining time");
+                /**
+                 * Add your policy specific initialization code here (if needed)
+                 */
+                break;
+            case HRRN:    //Highest response ratio next
+                System.out.println("Starting new scheduling task: Highest response ratio next");
+                /**
+                 * Add your policy specific initialization code here (if needed)
+                 */
+                break;
+            case FB:    //Feedback
+                System.out.println("Starting new scheduling task: Feedback, quantum = " + quantum);
+                /**
+                 * Add your policy specific initialization code here (if needed)
+                 */
+                break;
+        }
 
-		switch(policy) {
-		case FCFS:	//First-come-first-served
-			System.out.println("Starting new scheduling task: First-come-first-served");
-			/**
-			 * Add your policy specific initialization code here (if needed)
-			 */
-			break;
-		case RR:	//Round robin
-			System.out.println("Starting new scheduling task: Round robin, quantum = " + quantum);
-			/**
-			 * Add your policy specific initialization code here (if needed)
-			 */
-			break;
-		case SPN:	//Shortest process next
-			System.out.println("Starting new scheduling task: Shortest process next");
-			/**
-			 * Add your policy specific initialization code here (if needed)
-			 */
-			break;
-		case SRT:	//Shortest remaining time
-			System.out.println("Starting new scheduling task: Shortest remaining time");
-			/**
-			 * Add your policy specific initialization code here (if needed)
-			 */
-			break;
-		case HRRN:	//Highest response ratio next
-			System.out.println("Starting new scheduling task: Highest response ratio next");
-			/**
-			 * Add your policy specific initialization code here (if needed)
-			 */
-			break;
-		case FB:	//Feedback
-			System.out.println("Starting new scheduling task: Feedback, quantum = " + quantum);
-			/**
-			 * Add your policy specific initialization code here (if needed)
-			 */
-			break;
-		}
+        /**
+         * Add general scheduling or initialization code here (if needed)
+         */
 
-		/**
-		 * Add general scheduling or initialization code here (if needed)
-		 */
+    }
 
-	}
+    /**
+     * DO NOT CHANGE DEFINITION OF OPERATION
+     */
+    public void processAdded(int processID)
+    {
 
-	/**
-	 * DO NOT CHANGE DEFINITION OF OPERATION
-	 */
-	public void processAdded(int processID) {
+        /**
+         * Add scheduling code here
+         */
 
-		/**
-		 * Add scheduling code here
-		 */
+    }
 
-	}
+    /**
+     * DO NOT CHANGE DEFINITION OF OPERATION
+     */
+    public void processFinished(int processID)
+    {
 
-	/**
-	 * DO NOT CHANGE DEFINITION OF OPERATION
-	 */
-	public void processFinished(int processID) {
+        /**
+         * Add scheduling code here
+         */
 
-		/**
-		 * Add scheduling code here
-		 */
-
-	}
+    }
 }
